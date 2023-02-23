4cdaf218f959dca49d389921a1a4766da2d8a65d
MichalPlacek <michal.placek@yandex.com>
2023-02-23 21:33:51 +0100

    sumneko_lua -> lua_ls

--- config/lua/lsp-lua-config.lua
+++ config/lua/lsp-lua-config.lua
@@ -5,7 +5,7 @@ table.insert(runtime_path, "lua/?/init.lua")
 local sumneko_binary_path = vim.fn.exepath('lua-language-server')
 local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
 
-require'lspconfig'.sumneko_lua.setup {
+require'lspconfig'.lua_ls.setup {
   cmd = {sumneko_binary_path, "-E", sumneko_root_path .. "/main.lua"};
   settings = {
     Lua = {
